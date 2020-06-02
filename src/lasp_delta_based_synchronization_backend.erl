f91c4477528a1a4e50ad3ffd5583bb3a1f37bcc0
vintej <vinayaktj@gmail.com>
2020-06-02 16:38:45 +0200

    Debugging for multiple c1 bugfix optimized

--- src/lasp_delta_based_synchronization_backend.erl
+++ src/lasp_delta_based_synchronization_backend.erl
@@ -250,7 +250,7 @@ handle_cast({find_sub_aq, Id, ToNode, From}, #state{store=Store}=State) ->
             case ets:member(peer_rates, ToNode) of
                 true -> lager:error("LASPVIN ToNode ~p is a Peer.. Skipping ~n", [ToNode]), ok;
                 false -> 
-                    case lists:member(ToNode, ets:lookup_element(find_sub_aq, Id, 2)) of
+                    case lists:member(ToNode, ets:lookup_element(c1, "pseudopeer", 2)) of
                         true -> lager:error("LASPVIN path ToNode: ~p exists ~n",[ToNode]);
                         false -> found_sub_aq_lockpath(Id, ToNode, From)
                     end
@@ -840,6 +840,7 @@ found_sub_aq_lockpath(Id, ToNode, From) ->
                 true ->
                     lager:error("LASPVIN Got path to ~p ID:~p ~n", [ToNode, Id]),
                     ets:insert(find_sub_aq, [{Id, ToNode, From}]),
+                    ets:insert(c1, [{"pseudopeer", ToNode}]),
                     ?SYNC_BACKEND:send(?MODULE, {find_sub_aq_lock, Id, lasp_support:mynode()}, From);
                 false -> 
                     ets:insert(find_sub_aq, [{Id, ToNode, From}]),
