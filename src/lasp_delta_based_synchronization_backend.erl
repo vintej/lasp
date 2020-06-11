c12f02aa9888006feac117d828868fd293563dad
vintej <vinayaktj@gmail.com>
2020-06-11 10:05:35 +0200

    Debug 14

--- src/lasp_delta_based_synchronization_backend.erl
+++ src/lasp_delta_based_synchronization_backend.erl
@@ -292,6 +292,9 @@ handle_cast({find_sub_aq_lock, Id, From}, #state{store=Store}=State) ->
             %?SYNC_BACKEND:send(?MODULE, {rate_subscribe, lasp_support:mynode(), ets:lookup_element(peer_rates, "self_rate", 2)}, lists:nth(1, ets:lookup_element(c1, "peer", 2)))
             %propagate update_rate for all? may be at the end of the function,
             forward_aq_lock(Id, From),
+            timer:sleep(5),
+            lager:error("Deleting this rate_ack ~p ~n", [ets:tab2list(rate_ack)]),
+            lager:error("Rate_ack shown and this are the peers ~p ~n", [get_peers()]),
             ets:delete_all_objects(rate_ack)
     end,
     %ets:delete_object(find_sub, lists:nth(1,ets:match_object(find_sub, {'_', Id, '_'}))),
@@ -511,7 +514,9 @@ handle_info(rate_info, #state{store=Store}=State) ->
     lists:foreach(fun(Peer) ->
                         case ets:member(rate_ack, Peer) of
                            true -> ok;
-                           false -> ?SYNC_BACKEND:send(?MODULE, {rate_class, lasp_support:mynode(), ets:lookup_element(peer_rates, "self_rate", 2)}, Peer)
+                           false -> 
+                               lager:error("Sending rate ~p to ~p ~n", [ets:lookup_element(peer_rates, "self_rate", 2), Peer]),
+                               ?SYNC_BACKEND:send(?MODULE, {rate_class, lasp_support:mynode(), ets:lookup_element(peer_rates, "self_rate", 2)}, Peer)
                         end
                   end, 
                   Peers),
