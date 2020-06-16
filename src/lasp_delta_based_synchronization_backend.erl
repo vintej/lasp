8dcc1e97049fb9babd124a6c17b8c972ad5f2627
vintej <vinayaktj@gmail.com>
2020-06-16 22:40:58 +0200

    Removed reverse informing lock TBD

--- src/lasp_delta_based_synchronization_backend.erl
+++ src/lasp_delta_based_synchronization_backend.erl
@@ -1009,8 +1009,8 @@ check_sub_exists(From, ReqRate, Id, Hop) ->
                 lager:error("LASPVINDEBUG Matching find_sub rates found~n"),
                 lager:debug("LASPVIN find_sub:insert ReqRate:~p Id:~p From:~p ~n", [ReqRate, Id, From]),
                 %%%ERROR HERE
-                lager:error("LASPVINDEBUG Informing ~p that found peer for ID:~ toNode: ~p Via: ~p HopCount: ~p +1 ~n", [lists:nth(1,ets:lookup_element(find_sub, ReqRate, 3)), lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2)), string:substr(Id, 1, string:len(Id)-2), From, Hop]),
-                found_sub(lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2)), erlang:list_to_atom(string:substr(Id, 1, string:len(Id)-2)), From, Hop+1),
+                %lager:error("LASPVINDEBUG Informing ~p that found peer for ID:~ toNode: ~p Via: ~p HopCount: ~p +1 ~n", [lists:nth(1,ets:lookup_element(find_sub, ReqRate, 3)), lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2)), string:substr(Id, 1, string:len(Id)-2), From, Hop]),
+                %To be implemented found_sub(lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2)), erlang:list_to_atom(string:substr(Id, 1, string:len(Id)-2)), From, Hop+1),
                 ets:insert(match_sub_aq, [{Id, lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2))}]),
                 ets:insert(match_sub_aq, [{lists:nth(1, ets:lookup_element(find_sub, ReqRate, 2)), Id}]),
                 insert_findSub(ReqRate, Id, From, Hop),
